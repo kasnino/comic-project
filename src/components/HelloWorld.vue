3004c4af323bae8575e444b83bc06fd14d5be389
= <=>
2022-03-27 04:30:27 -0400

    add action fetch api

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,6 +1,6 @@
 <script setup>
  import { useStore } from 'vuex'
- import { ref, computed } from 'vue'
+ import { ref, computed, onMounted } from 'vue'
 
   defineProps({
     msg: String
@@ -9,24 +9,46 @@
   const store = useStore()
 
   function incrementCount() {
-      store.commit('increment')
+      store.commit('INCREMENT_COMIC')
   }
 
   function decrementCount() {
-      store.commit('decrement')
+      store.commit('DECREMENT_COMIC')
   }
 
-   const count = computed(() => store.state.count);
+  const count  = computed(() => store.state.count);
+  const comics = computed(() => store.state.comics);
+
+  onMounted(() => {
+   store.dispatch('GET_COMICS');
+  })
 </script>
 
 <template>
-  <h1 class="text-cyan">{{ msg }}</h1>
- <h1 class="text-3xl font-bold text-white ">
-    Hello world!
+<div class="grid grid-cols-1 gap-4">
+  <div class=" text-center p-4 h-auto">
+
+
+
+      <pre v-if="comics">
+        {{comics}}
+      </pre>
+       <h1 class="text-3xl font-bold text-white ">
   </h1>
-  <button type="button" @click="incrementCount">+</button>
+  <button @click="incrementCount" class="bg-pink-500  hover:bg-purple-400 text-white font-bold py-2 px-4 border-b-4 border-purple-700 hover:border-purple-500 rounded">
+  +
+</button>
+ 
    count is: {{ count }}
-  <button type="button" @click="decrementCount">-</button>
+  
+  <button @click="decrementCount" class="bg-pink-500  hover:bg-purple-400 text-white font-bold py-2 px-4 border-b-4 border-purple-700 hover:border-purple-500 rounded">
+    -</button>
+
+
+  </div>
+
+</div>
+
 
 </template>
 
--- src/store/index.js
+++ src/store/index.js
@@ -3,17 +3,43 @@ import { createStore } from "vuex";
 export default createStore({
   state () {
     return {
-      count: 0
-    }
+      count: 1,
+      comics: []
+     }
   },
   mutations: {
-    increment (state) {
+    INCREMENT_COMIC (state) {
       state.count++
     },
 
-    decrement (state) {
-      if(state.count != 0)
+    DECREMENT_COMIC (state) {
+      if(state.count != 1)
       state.count--
+    },
+
+    SET_COMICS (state, payload){
+      state.comics = payload
+    }
+  },
+  actions:{
+    
+   async GET_COMICS({commit}){
+    var config = {
+      headers: {
+           'Content-type': 'text/plain',
+           'Access-Control-Allow-Headers': '*',
+           'Access-Control-Allow-Origin':'*',
+           'Access-Control-Allow-Methods':'GET',
+         }
+   };
+      try {
+        const res  = await fetch('https://xkcd.com/500/info.0.json',config)
+        const data = await res.json()
+        commit('SET_COMICS', JSON.stringify(data))
+       
+      } catch (error) {
+        console.log("Error en el actions get commits")
+      }
     }
   }
 })
